3d10e8385b8bf528c4e130112b6563c540738d48
LeviSY <49865069+LeviSY@users.noreply.github.com>
2020-07-11 17:59:59 +0300

    Add files via upload

---
+++ LR22/LR22/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace LR22
+{
+    class Program
+    {
+        static int MinPoloj(int[] mas)
+        {
+            int num = int.MaxValue;
+            for (int i = 0; i < mas.Length; i++)
+            {
+                if (mas[i] > 0 && mas[i] < num)
+                    num = mas[i];
+            }
+
+            return num;
+        }
+
+        static void Main(string[] args)
+        {
+            int min = 10;
+            int[] mas = new int[10];
+            Random r = new Random();
+
+            Console.WriteLine("Случайно сгенерированые числа");
+
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas[i] = r.Next(-10, 10);
+                Console.Write(mas[i] + " \t ");
+            }
+
+            Console.WriteLine();
+            Console.ReadKey();
+
+            Console.WriteLine("Минимальное положительное число: ");
+
+            for (int i = 0; i < mas.Length; i++)
+            {
+                if (mas[i] < min & mas[i] > 0)
+                {
+                    min = mas[i];
+                }
+            }
+            Console.WriteLine(min);
+            Console.ReadKey();
+        }
+    }
+}
